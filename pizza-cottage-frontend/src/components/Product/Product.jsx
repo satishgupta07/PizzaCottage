fb341cb12b6656d32bf3aacaf5bb5afef9fa334b
Satish Kumar <satish.kumar06@nagarro.com>
2022-12-20 23:50:36 +0530

    Home Page and Product List
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- pizza-cottage-frontend/src/components/Product/Product.jsx
+++ pizza-cottage-frontend/src/components/Product/Product.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <div>
+      <img src='/images/peproni.png' alt='pizza'/>
+      <div className='text-center'>
+        <h2 className='text-lg font-bold py-2'>Havana Special</h2>
+        <span className='bg-gray-200 py-1 rounded-full text-sm px-4'>Small</span>
+      </div>
+      <div className='flex justify-between items-center mt-4'>
+        <span>₹ 500</span>
+        <button className='bg-yellow-500 py-1 px-4 rounded-full font-bold'>ADD</button>
+      </div>
+    </div>
   )
 }
 
