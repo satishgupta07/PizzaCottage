050be78057914113623c635edfffb05568d5586b
Satish Kumar <satish.kumar06@nagarro.com>
2022-12-21 09:43:21 +0530

    useState(), useEffect() Hooks and props
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- pizza-cottage-frontend/src/components/Products/Products.jsx
+++ pizza-cottage-frontend/src/components/Products/Products.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Product from '../Product/Product';
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://star-spark-pasta.glitch.me/api/products')
+    .then(ressponse => ressponse.json())
+    .then(products => {
+      setProducts(products);
+    })
+  },[]);
+
   return (
     <div className='container mx-auto pb-24'>
       <h1 className='text-lg font-bold my-8'>Products</h1>
       <div className='grid grid-cols-5 my-8 gap-24'>
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+          {
+            products.map(product => <Product key={product._id} product={product} />)
+          }
       </div>
     </div>
   )
