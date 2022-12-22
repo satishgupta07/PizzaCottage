9c718a51b12f8f31482526752e999a2dc884a1c0
Satish Kumar <satish.kumar06@nagarro.com>
2022-12-22 20:52:29 +0530

    Routes | Controller | Middleware | Error Handling
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- pizza-cottage-backend/server.js
+++ pizza-cottage-backend/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require('express');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 const PORT = process.env.PORT || 3333
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(express.json());
+app.use('/api',require('./routes'))
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
